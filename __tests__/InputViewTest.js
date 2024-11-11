2fec2992f26660338d1b3c39b1ba112fed6a0968
ShinjungOh <tlswjd9260@gmail.com>
2024-11-11 20:48:51 +0900

    test: InputViewTest의 Y/N 입력값 유효성 검사 테스트 코드 작성

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -19,16 +19,16 @@ const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync.messages = messages;
 };
 
-describe("InputView 클래스 입력값 유효성 테스트", () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-    jest.restoreAllMocks();
+describe("InputView 클래스 Cart 입력값 유효성 테스트", () => {
+  test("올바른 형식으로 입력한 경우", async () => {
+    mockQuestions(['[콜라-10],[사이다-3]']);
+    await expect(InputView.readLineAddCartItemList()).resolves.not.toThrow();
   });
 
   test("형식이 맞지 않는 경우 예외 처리한다. - 대괄호가 없는 경우", async () => {
     mockQuestions(['콜라-10],[사이다-3']);
     try {
-      await InputView.addCart();
+      await InputView.readLineAddCartItemList();
     } catch (error) {
       expect(error.message).toBe(MESSAGES.error.productAndCountForm);
     }
@@ -37,7 +37,7 @@ describe("InputView 클래스 입력값 유효성 테스트", () => {
   test("형식이 맞지 않는 경우 예외 처리한다. - 수량의 형식이 숫자가 아닌 경우", async () => {
     mockQuestions(['[콜라-ㅇ]']);
     try {
-      await InputView.addCart();
+      await InputView.readLineAddCartItemList();
     } catch (error) {
       expect(error.message).toBe(MESSAGES.error.productAndCountForm);
     }
@@ -46,7 +46,7 @@ describe("InputView 클래스 입력값 유효성 테스트", () => {
   test("형식이 맞지 않는 경우 예외 처리한다. - 수량의 형식이 올바르지 않은 경우", async () => {
     mockQuestions(['[콜라-"1"]']);
     try {
-      await InputView.addCart();
+      await InputView.readLineAddCartItemList();
     } catch (error) {
       expect(error.message).toBe(MESSAGES.error.productAndCountForm);
     }
@@ -55,9 +55,30 @@ describe("InputView 클래스 입력값 유효성 테스트", () => {
   test("형식이 맞지 않는 경우 예외 처리한다. - 수량이 0인 경우", async () => {
     mockQuestions(['[콜라-0]']);
     try {
-      await InputView.addCart();
+      await InputView.readLineAddCartItemList();
     } catch (error) {
       expect(error.message).toBe(MESSAGES.error.productAndCountForm);
+    }
+  });
+});
+
+describe("InputView 클래스 Y/N 입력값 유효성 테스트", () => {
+  test("Y로 답변하기 - 멤버십을 적용하는 경우", async () => {
+    mockQuestions(['Y']);
+    await expect(InputView.applyMembership()).resolves.not.toThrow();
+  });
+
+  test("N으로 답변하기 - 멤버십을 적용하지 않는 경우", async () => {
+    mockQuestions(['N']);
+    await expect(InputView.applyMembership()).resolves.not.toThrow();
+  });
+
+  test("형식이 맞지 않는 경우 예외 처리한다. - Y 또는 N가 아닌 경우", async () => {
+    mockQuestions(['abc']);
+    try {
+      await InputView.applyMembership();
+    } catch (error) {
+      expect(error.message).toBe(MESSAGES.error.invalidInput);
     }
   });
 });
